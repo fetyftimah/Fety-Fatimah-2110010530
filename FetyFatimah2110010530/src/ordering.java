176a92737f47d4bfac016fd0428c25c54b989631
fetyftimah <56721787+fetyftimah@users.noreply.github.com>
2023-05-16 10:39:46 +0700

    Add files via upload

---
+++ FetyFatimah2110010530/src/ordering.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Fety Fatimah
+ */
+public class ordering {
+    private int noPemesanan;
+    private int kodePelanggan;
+    private String tglPemesanan;
+    private String namaPenerima;
+    private String namaLengkap;
+    private int kodePos;
+    private int noTelp;
+    private boolean statusByr;
+    private boolean statusKirim;
+    
+    public ordering(String value){
+        System.out.println(value);
+    }
+    
+}
